3e375b41746192dbc65092b7cb1859f659808e64
ArduSan <ferretti2003simone@gmail.com>
2025-05-20 13:55:56 +0200

    MainMenuView update

--- app/src/main/java/com/example/simon_kotlords/ui/theme/Color.kt
+++ app/src/main/java/com/example/simon_kotlords/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF121212)
+val DarkPrimary = Color(0xFFBB86FC)
+val DarkSecondary = Color(0xFF03DAC5)
+val DarkOnBackground = Color.White
+val DarkOnSurface = Color.White
