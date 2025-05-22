4570964f85e7d74b032cc0736899a070305d5a91
ArduSan <ferretti2003simone@gmail.com>
2025-05-22 18:33:40 +0200

    Gradle Updated for Navigation and set different things for navigation implementation

--- app/src/main/java/com/example/simon_kotlords/MainActivity.kt
+++ app/src/main/java/com/example/simon_kotlords/MainActivity.kt
@@ -11,7 +11,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.simon_kotlords.ui.theme.SimonKotlordsTheme
+import com.example.simon_kotlords.ui.view.MainMenuView
+
+object AppDestinations {
+    const val MAIN_MENU_ROUTE = "main_menu"
+    const val PLAY_GAME_ROUTE = "play_game"
+    const val HIGHLIGHTS_ROUTE = "highlights"
+    const val CREDITS_ROUTE = "credits"
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +30,36 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimonKotlordsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                //}
             }
         }
     }
 }
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = AppDestinations.MAIN_MENU_ROUTE
+    ) {
+        composable(AppDestinations.MAIN_MENU_ROUTE) {
+            MainMenuView(
+                onPlayClicked = { navController.navigate(AppDestinations.PLAY_GAME_ROUTE) },
+                //onHighlightsClicked = { navController.navigate(AppDestinations.HIGHLIGHTS_ROUTE) },
+                //onCreditsClicked = { navController.navigate(AppDestinations.CREDITS_ROUTE) }
+            )
+        }
+        /*
+        composable(AppDestinations.PLAY_GAME_ROUTE) {GameView()}
+        composable(AppDestinations.HIGHLIGHTS_ROUTE){HighlightsView()}}
+        composable(AppDestinations.CREDITS_ROUTE){CreditsView()}
+        */
+    }
+
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
