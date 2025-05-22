7302840772c26f861870ca730d36ecd69d87d944
underAlex <alessandro.libotte@gmail.com>
2025-05-23 00:47:31 +0200

    Update to GameView.kt and MainActivity.kt, refactor of CreditsView.kt and HighlightsView.kt for code consistency

--- app/src/main/java/com/example/simon_kotlords/ui/view/GameView.kt
+++ app/src/main/java/com/example/simon_kotlords/ui/view/GameView.kt
@@ -2,17 +2,21 @@ package com.example.simon_kotlords.ui.view
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -23,56 +27,83 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.painterResource
+import com.example.simon_kotlords.R
+import com.example.simon_kotlords.ui.theme.SimonBlue
+import com.example.simon_kotlords.ui.theme.SimonGreen
+import com.example.simon_kotlords.ui.theme.SimonKotlordsTheme
+import com.example.simon_kotlords.ui.theme.SimonRed
+import com.example.simon_kotlords.ui.theme.SimonYellow
 
 @Composable
 fun GameView(modifier: Modifier = Modifier){
 
-    Column(
+    Surface(
         modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
-    )
-    {
-
-        Text(
-            "Fai attenzione!",
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.primary
-        )
+        color= MaterialTheme.colorScheme.background
+    ) {
 
         Column(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                ArcButton(Color.Red, 180f, 90f, (50 / 2).dp, (50 / 2).dp)
+            Text(
+                "Fai attenzione!",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
 
-                ArcButton(Color.Blue, 270f, 90f, (-90).dp, (50 / 2).dp)
+            Box(
+                contentAlignment = Alignment.Center,
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.game_logo_purple),
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(360.dp)
+                )
+
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        ArcButton(SimonRed, 180f, 90f, 35.dp, 35.dp)
+
+                        ArcButton(SimonGreen, 270f, 90f, (-65).dp, 35.dp)
+                    }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        ArcButton(SimonYellow, 90f, 90f, 35.dp, (-65).dp)
+
+                        ArcButton(SimonBlue, 0f, 90f, (-65).dp, (-65).dp)
+                    }
+
+
+                }
             }
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                ArcButton(Color.Green, 90f, 90f, (50 / 2).dp, (-90).dp)
-
-                ArcButton(Color.Yellow, 0f, 90f, (-90).dp, (-90).dp)
-            }
-
+            Text(
+                "Livello 1",
+                style = MaterialTheme.typography.headlineLarge,
+                color = MaterialTheme.colorScheme.primary
+            )
 
         }
-
-        Text(
-            "Livello 1",
-            style = MaterialTheme.typography.headlineLarge,
-            color = MaterialTheme.colorScheme.primary
-        )
-
     }
 
 }
@@ -89,31 +120,46 @@ fun ArcButton(
 
     val interactionSource = remember { MutableInteractionSource()}
 
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val animatedColor = if (isPressed) {
+        color.copy(alpha = color.alpha,
+            red = (color.red + (1f - color.red) * 0.4f).coerceIn(0f, 1f),
+            green = (color.green + (1f - color.green) * 0.4f).coerceIn(0f, 1f),
+            blue = (color.blue + (1f - color.blue) * 0.4f).coerceIn(0f, 1f)
+        )
+    } else {
+        color
+    }
+
     Canvas (
     modifier = Modifier
-        .size(120.dp, 120.dp)
+        .size(125.dp, 125.dp)
         .clickable(
             interactionSource = interactionSource,
             onClick = onClick,
-            indication = ripple(bounded = false, radius = 1.dp)
+            indication = null
         ),
     ){
 
         drawArc(
-            color = color,
+            color = animatedColor,
             startAngle = startAngle,
             sweepAngle = sweepAngle,
             useCenter = false,
-            size = Size(180.dp.toPx(), 180.dp.toPx()),
+            size = Size(155.dp.toPx(), 155.dp.toPx()),
             topLeft = Offset(offsetX.toPx(), offsetY.toPx()),
-            style = Stroke(width = 50.dp.toPx())
+            style = Stroke(width = 55.dp.toPx()),
+
         )
     }
 
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun GameViewPreview(){
-    GameView()
+    SimonKotlordsTheme {
+        GameView()
+    }
 }
--- app/src/main/java/com/example/simon_kotlords/ui/view/HighlightsView.kt
+++ app/src/main/java/com/example/simon_kotlords/ui/view/HighlightsView.kt
@@ -1,18 +1,12 @@
 package com.example.simon_kotlords.ui.view
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,28 +15,15 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
-fun HighlightsView(onNavigateBack: () -> Unit,
-                   modifier: Modifier = Modifier
-) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text("HighLights") },
-                navigationIcon = {
-                    IconButton(onClick = onNavigateBack) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Back"
-                        )
-                    }
-                }
-            )
-        },
-        modifier = modifier
-    ) { innerPadding ->
-        Column(
+fun HighlightsView(modifier: Modifier = Modifier) {
+
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color= MaterialTheme.colorScheme.background
+    ) {
+
+        LazyColumn(
             modifier = Modifier
-                .padding(innerPadding)
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
