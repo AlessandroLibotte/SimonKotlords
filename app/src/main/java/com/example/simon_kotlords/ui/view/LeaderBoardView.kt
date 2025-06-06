b4b1606e6927fb68e0df6477e26b4b8203b888bf
underAlex <alessandro.libotte@gmail.com>
2025-06-06 10:48:00 +0200

    Changed project structure to add LeaderBoardRepository.kt, added Hilt plugin for dependency injection, implemented LeaderBoardViewModel.kt and GameViewModel.kt communication

--- app/src/main/java/com/example/simon_kotlords/ui/view/LeaderBoardView.kt
+++ app/src/main/java/com/example/simon_kotlords/ui/view/LeaderBoardView.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -19,13 +18,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.simon_kotlords.data.model.HighScoreEntity
 import com.example.simon_kotlords.ui.model.LeaderBoardViewModel
 import com.example.simon_kotlords.ui.theme.SimonKotlordsTheme
-import java.text.SimpleDateFormat
 import java.time.format.DateTimeFormatter
-import java.util.Locale
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
